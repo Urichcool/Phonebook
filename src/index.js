8531c5bd5ed019cad6e3cd90eae22f7f50e90db9
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2023-01-31 00:19:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         }
         persistor={persistor}
       >
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+        <BrowserRouter basename="/Phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
